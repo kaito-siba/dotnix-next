70aabcdf88e3920c8b60fe48c26affef6aafeac8
k-nanchi <kaito@siba-service.jp>
2026-08-25 15:57:43 +0900

    fix: intelephense が memory 不足で落ちる問題を修正

--- modules/dev/php.nix
+++ modules/dev/php.nix
@@ -3,6 +3,21 @@
 
   flake.modules.homeManager.dev =
     { pkgs, ... }:
+    let
+      # intelephense はプロジェクトのインデックスを全てメモリに載せるため、
+      # node の old-space デフォルト上限 (~4GB) だと大きなリポジトリで OOM する。
+      # 本体は unfree でローカルビルドになるので、overrideAttrs による
+      # 再ビルドを避けて symlinkJoin でラップするだけにしている。
+      intelephense = pkgs.symlinkJoin {
+        name = "intelephense-wrapped";
+        paths = [ pkgs.intelephense ];
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+        postBuild = ''
+          wrapProgram $out/bin/intelephense \
+            --set NODE_OPTIONS "--max-old-space-size=8192"
+        '';
+      };
+    in
     {
       home.packages = with pkgs; [
         intelephense
