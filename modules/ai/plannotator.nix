34cbd51866b2fdd33c2a18d769aab642b29f0de7
k-nanchi <kaito@siba-service.jp>
2026-09-12 13:44:06 +0900

    feat: plannotator の mac 対応

--- modules/ai/plannotator.nix
+++ modules/ai/plannotator.nix
@@ -183,6 +183,7 @@
               "x86_64-linux"
               "aarch64-linux"
               "aarch64-darwin"
+              "x86_64-darwin"
             ];
             sourceProvenance = [ lib.sourceTypes.binaryNativeCode ];
           };
